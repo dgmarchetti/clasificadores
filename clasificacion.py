7530b6ab67880c52b998dcf216922427d59bd079
Diego German Marchetti <diego.marchetti@mercadolibre.com>
2019-10-21 00:07:50 -0300

    version 1.0.0

---
+++ clasificacion.py
@@ -0,0 +1,51 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+from NeuralNetwork import NeuralNetwork
+import data_io as io
+
+def correcto(s, p):
+    if p > 0.5 and s == 1:
+        return 1
+    elif p <= 0.5 and s == 0:
+        return 1
+    return 0
+
+layers = [5, 7, 3, 1]
+nro_epochs = 3000000
+index=0
+correctos=0
+
+# Datos TP
+nn = NeuralNetwork(layers)
+x = io.get_x_data().values
+y = io.get_y_data().values
+nn.fit(x, y,epochs=nro_epochs)
+
+for e in x:
+    predicted = nn.predict(e)
+    bien = correcto(y[index][0], predicted[0])
+    correctos += bien
+    print("Entradas:", e, "Salida:", y[index], "Predicción:", predicted, "Bien: ", bien, "Correctos:", correctos)
+    index += 1
+
+print("Layers:", layers, "Epochs:",nro_epochs)
+print("Correctos " + str(correctos) + " sobre " + str(index) + " "+ str(correctos/(index)*100) + "%.")
+
+
+# Imprimir los pesos de las conexiones
+nn.print_weights()
+
+# Graficamos la función coste
+# deltas = nn.get_deltas()
+# valores=[]
+# index=0
+# for arreglo in deltas:
+#     valores.append(arreglo[1][0] + arreglo[1][0])
+#     index=index+1
+#
+# plt.plot(range(len(valores)), valores, color='b')
+# plt.ylim([0, 1])
+# plt.ylabel('Cost')
+# plt.xlabel('Epochs')
+# plt.tight_layout()
+# plt.show()
--- data_io.py
+++ data_io.py
@@ -1,23 +1,23 @@
 import pandas as pd
-data_file_Xtrain = "./datasets/train/X_train.csv"
-data_file_Ytrain = "./datasets/train/Y_train.csv"
 
-# Create a list of the new column labels: new_labels
-Xlabels = ['X1', 'X2', 'X3', 'X4', 'X5']
-Ylabels = ['Y']
 
-# Read in the file, specifying the header and names parameters: xTrain
-xTrain = pd.read_csv(data_file_Xtrain, names=Xlabels)
-yTrain = pd.read_csv(data_file_Ytrain, names=Ylabels)
+def get_x_data():
+    data_file_xtrain = "./datasets/train/X_train.csv"
+    # Create a list of the new column labels: new_labels
+    xlabels = ['X1', 'X2', 'X3', 'X4', 'X5']
+    # Read in the file, specifying the header and names parameters: xtrain
+    xtrain = pd.read_csv(data_file_xtrain, names=xlabels)
+    # print(xtrain.head(10))
+    # print(xtrain.info())
+    return xtrain
 
-# Print both the DataFrames head and info
-# print(xTrain.head(10))
-print(xTrain.info())
-# print(yTrain.head(10))
-print(yTrain.info())
-# print(xTrain.describe())
 
-# Armar vector patrón aumentado
-# xTrain['W6'] = 1
-# print(xTrain.head(10))
-
+def get_y_data():
+    data_file_ytrain = "./datasets/train/Y_train.csv"
+    # Create a list of the new column labels: new_labels
+    ylabels = ['Y']
+    # Read in the file, specifying the header and names parameters: ytrain
+    ytrain = pd.read_csv(data_file_ytrain, names=ylabels)
+    # print(ytrain.head(10))
+    # print(ytrain.info())
+    return ytrain
